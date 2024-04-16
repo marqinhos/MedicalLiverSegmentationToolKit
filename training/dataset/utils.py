b77658b2e4dacc1b3dc8241bada68fae1417cf6c
Marcos <marcosf12400@gmail.com>
2024-04-16 12:03:34 +0200

    Add comment to the function

--- training/dataset/utils.py
+++ training/dataset/utils.py
@@ -1,10 +1,18 @@
 def get_dataset(args, **kwargs):
+    """Function to get the specific dataset
+
+    Args:
+        args (argparse.Namespace): Arguments from the command line.
+
+    Returns:
+        Dataset: The dataset object.
+    """    
     if args.dimension == "2d":
         assert args.dimension == "2d" "2D dataset is not implemented yet"
         pass
 
     else:
         if args.dataset == "btcv":
-            from .dim3.dataset_btcv2 import BTCVDataset
-
+            from .dim3.dataset_btcv import BTCVDataset
+            print(type(BTCVDataset(args)))
             return BTCVDataset(args)
