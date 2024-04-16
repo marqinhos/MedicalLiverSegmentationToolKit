b4233cf2aa3eea82f5d592602648f75f5fc7e4ab
Marcos <marcosf12400@gmail.com>
2024-04-16 12:34:57 +0200

    Add comments

--- dataset_normalization/btcv.py
+++ dataset_normalization/btcv.py
@@ -4,6 +4,7 @@ import yaml
 import numpy as np
 import nibabel as nib
 import SimpleITK as sitk
+
 from monai.data import Dataset
 from monai.transforms import (
     LoadImaged, 
@@ -12,27 +13,33 @@ from monai.transforms import (
     EnsureChannelFirstd, 
     Orientationd,
     ScaleIntensityRanged,
-    CropForegroundd,
-    Invertd,
-    Resized,
-    SaveImaged,
-    ScaleIntensityd,
-    RandAffine,
-
 )
 from sklearn.model_selection import train_test_split
+
 from utils import load_from_file
 
 
 class NormalizedDataset:
+    """Class to normalize a dataset
+    """    
 
-    def __init__(self) -> None:
+    def __init__(self):
+        """Constructor of the NormalizedDataset class.
+        """        
         self.path_img_list = {}
         self.path_label_list = {}
         self.img_names_list = None
         self.keys = ["image", "label"]
 
+
     def __call__(self, src_path, tgt_path, train_percentage):
+        """Function to normalize the dataset and save it in the target path.
+
+        Args:
+            src_path (str): Full path to the source dataset.
+            tgt_path (str): Full path to the target dataset.
+            train_percentage (float): Percentage of the dataset to be used for training.
+        """        
         name_list_ext_tr = os.listdir(src_path + "/imagesTr")
         name_list_ext_ts = os.listdir(src_path + "/imagesTs")
 
@@ -59,46 +66,16 @@ class NormalizedDataset:
 
         self.__apply_transformations(train_img_names, tgt_path, "")
         
-        
-
-
-    def __apply_transformations(self, data, tgt_path, sub_folder):
-        for img_name in data:
-            lbl_name = img_name.replace("img", "label")
-
-            img = nib.load(self.path_img_list[img_name])
-            lbl = nib.load(self.path_label_list[lbl_name])
-
-            print(img.get_fdata().shape)
-            #data = {self.keys[0]: img.get_fdata(), self.keys[1]: lbl.get_fdata()}
-            transformation = self.transformations(img.header.get_zooms())
-            data_dict = [{self.keys[0]: self.path_img_list[img_name], self.keys[1]: self.path_label_list[lbl_name]}]
-            ## Important data_dict be a list of dictionaries
-            #img = LoadImaged(self.keys)
-            dataset_0 = Dataset(data=data_dict, transform=transformation)
-            #print(dataset_0.data)
-            #print(dataset_0.transform)
-
-            img_normalized = dataset_0[0][self.keys[0]].squeeze().get_array()
-            lbl_normalized = dataset_0[0][self.keys[1]].squeeze().get_array()
-            print(img_normalized.shape)
-            
-            img_name = img_name.replace(".nii.gz", "")
-            lbl_name = lbl_name.replace(".nii.gz", "")
-            #data = transformation(data) # data[self.keys[1]]
-
-            sitk_img = sitk.GetImageFromArray(img_normalized)
-            sitk_lbl = sitk.GetImageFromArray(lbl_normalized)
-
-            sitk.WriteImage(sitk_img, '%s/%s.nii.gz'%(tgt_path, img_name))
-            sitk.WriteImage(sitk_lbl, '%s/%s_gt.nii.gz'%(tgt_path, img_name))
-            print(img_name, 'done')
-            print(lbl_name, 'done')
-
 
     def transformations(self, pixdim):
-        print(pixdim)
-        pix = min(pixdim)
+        """Function to apply the transformations to the dataset.
+
+        Args:
+            pixdim (tuple): Tuple with the pixel dimensions.
+
+        Returns:
+            monai.transforms.compose.Compose: Compose object with the transformations to be applied.
+        """        
         return Compose(
                     [
                     LoadImaged(keys=self.keys, image_only=True),
@@ -117,15 +94,59 @@ class NormalizedDataset:
                         b_max=1.0,
                         clip=True,
                     ),
-                    #SaveImaged(keys=self.keys, output_dir=tgt_path)
-                
                     ]
                 )
 
-    def split_dataset(self, name_list_ext, test_percentage):
-        return train_test_split(name_list_ext, test_size=test_percentage, random_state=42)
-        
 
+    def split_dataset(self, name_list_ext, test_percentage):
+        """Function to split the dataset into training and testing.
+
+        Args:
+            name_list_ext (list): List with the names of the images.    
+            test_percentage (float): Percentage of the dataset to split it.
+
+        Returns:
+            list, list: Lists with the names of the images for training and testing. 
+        """        
+        return train_test_split(name_list_ext, test_size=test_percentage, random_state=42)
+    
+
+    def __apply_transformations(self, data, tgt_path):
+        """Function to apply the transformations to the dataset.
+
+        Args:
+            data (list): List with the data to be transformed.
+            tgt_path (str): Full path to the target dataset.
+        """        
+        for img_name in data:
+            lbl_name = img_name.replace("img", "label")
+
+            img = nib.load(self.path_img_list[img_name])
+            lbl = nib.load(self.path_label_list[lbl_name])
+
+            print(img.get_fdata().shape)
+            #data = {self.keys[0]: img.get_fdata(), self.keys[1]: lbl.get_fdata()}
+            transformation = self.transformations(img.header.get_zooms())
+            data_dict = [{self.keys[0]: self.path_img_list[img_name], self.keys[1]: self.path_label_list[lbl_name]}]
+            ## Important data_dict be a list of dictionaries
+            dataset_0 = Dataset(data=data_dict, transform=transformation)
+
+            img_normalized = dataset_0[0][self.keys[0]].squeeze().get_array()
+            lbl_normalized = dataset_0[0][self.keys[1]].squeeze().get_array()
+            print(img_normalized.shape)
+            
+            img_name = img_name.replace(".nii.gz", "")
+            lbl_name = lbl_name.replace(".nii.gz", "")
+
+            sitk_img = sitk.GetImageFromArray(img_normalized)
+            sitk_lbl = sitk.GetImageFromArray(lbl_normalized)
+
+            sitk.WriteImage(sitk_img, '%s/%s.nii.gz'%(tgt_path, img_name))
+            sitk.WriteImage(sitk_lbl, '%s/%s_gt.nii.gz'%(tgt_path, img_name))
+            print(img_name, 'done')
+            print(lbl_name, 'done')
+
+        
 
 if __name__ == "__main__":
     with open("paths.yaml", 'r') as stream:
@@ -140,7 +161,6 @@ if __name__ == "__main__":
 
     dataset = NormalizedDataset()
     dataset(src_path, tgt_path, train_percentage)
-
 
     print("Data normalized and saved successfully.")    
     
