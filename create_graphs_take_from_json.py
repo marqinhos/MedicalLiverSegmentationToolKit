f4e41f971919058493b41d4d86abaee74dde05d8
Marcos <marcosf12400@gmail.com>
2024-07-03 16:33:20 +0200

    Add more graphs

--- create_graphs_take_from_json.py
+++ create_graphs_take_from_json.py
@@ -54,10 +54,56 @@ def take_param():
     }
 
     for net_name in list(net_values_dict.keys()):
-        net_values_dict[net_name] = data[net_name]["num_param"][0]
+        net_values_dict[net_name] = data[net_name]["num_flops"][0]
 
     print(net_values_dict)
     print(net_values_dict.values())
     print(net_values_dict.keys())
 
-take_param()
+def take_measures():
+    with open('metrics.json', 'r') as file:
+        data = json.load(file)
+
+    net_values_dict = {
+        'resunet': {"mean": None, "list_all": None},
+        'unet++': {"mean": None, "list_all": None},
+        'attention_unet': {"mean": None, "list_all": None},
+        'unetr': {"mean": None, "list_all": None},
+        'swin_unetr': {"mean": None, "list_all": None},
+        'medformer': {"mean": None, "list_all": None},
+        'segformer': {"mean": None, "list_all": None}
+    }
+
+    final = {
+        'DSC': None,
+        'NSD': None,
+        'MASD': None,
+        'HD': None,
+        'RVD': None
+    }
+
+    for measure_name in list(final.keys()):
+        for net_name in list(net_values_dict.keys()):
+            net_values_dict[net_name]["mean"] = data[measure_name][net_name]["mean_value_metrics"]["Liver"]
+            list_keys = list(data[measure_name][net_name]["dataset"]["BTCV"].keys())
+            good_keys = [key for key in list_keys if "img" in key]
+            net_values_dict[net_name]["list_all"] = [data[measure_name][net_name]["dataset"]["BTCV"][key]["metrics"]["Liver"] for key in good_keys]
+
+        final[measure_name] = net_values_dict
+        net_values_dict = {
+            'resunet': {"mean": None, "list_all": None},
+            'unet++': {"mean": None, "list_all": None},
+            'attention_unet': {"mean": None, "list_all": None},
+            'unetr': {"mean": None, "list_all": None},
+            'swin_unetr': {"mean": None, "list_all": None},
+            'medformer': {"mean": None, "list_all": None},
+            'segformer': {"mean": None, "list_all": None}
+        }
+
+    with open('data_blox_plot.json', 'w') as f:
+        json.dump(final, f, indent=5)
+    print(final)
+
+#take_param()
+
+take_measures()
